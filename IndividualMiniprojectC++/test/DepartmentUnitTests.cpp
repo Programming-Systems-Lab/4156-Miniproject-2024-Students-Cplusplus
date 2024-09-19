87a858a81f9eb2295c28770681c8a66aa9f10c39
kelvin2kim <158856317+kelvin2kim@users.noreply.github.com>
2024-09-19 02:02:19 -0400

    Bug fixes (#3)
    * Made changes to include clang-tidy in CMakelists.txt. Added more links in citations.txt to account for the sources I used as I tried to navigate around installation issues with clang-tidy. Finally, added some additional text to README regarding the setup info, specifically regarding clang-tidy and how I installed and set everything up.
    
    * Changes to CMakeLists for code coverage tests
    
    * CMakeLists change
    
    * Managed to figure out how to set up coverage analysis tool for c++ using gcc. Made necessary edits to CMakeLists.txt and for now the part regarding static analysis with clang-tidy is commented out as it is too slow. Also added the necessary information to use the coverage analysis tool govc in README.
    
    * A few more tests added to CourseUnitTests.cpp
    
    * Edited CourseUnitTests to comply with cpplint and fixed some issues with Course.cpp that clang-tidy was warning about. Also documented these changes in bugs.txt
    
    * Added more details to bugs.txt, and fixed a bug in DepartmentUnitTests.cpp
    
    * Squashed more bugs for Department.cpp and MyFileDataBase.cpp, and documented the changes to bugs.txt. Also started added some details to citations.txt. And also finished with more bugs for Course.cpp
    
    * Finished up readme, citations.txt, as well as honesty.txt. Also removed unnecessary files in the build directory.
    
    * Bug fix on MyFileDatabase deSerializeObjectFromFile() function as it was not returning anything, also added the contents of the external libraries of the folder external_libraries in gitignore.
    
    * Added Serialize and Deserialize Tests to DepartmentUnitTests.cpp
    
    * Added test case for the last couple of methods in DepartmentUnitTests.cpp

--- IndividualMiniprojectC++/test/DepartmentUnitTests.cpp
+++ IndividualMiniprojectC++/test/DepartmentUnitTests.cpp
@@ -1,33 +1,51 @@
+#include <fstream>
 #include <gtest/gtest.h>
 #include "Department.h"
+
+auto course1 = std::make_shared<Course>(150,
+"Dr. Smith", "Room 101", "MWF 10:00-11:00 AM");
+auto course2 = std::make_shared<Course>(200,
+"Prof. Johnson", "Room 102", "TR 2:00-3:30 PM");
+auto course3 = std::make_shared<Course>(150, "Mr. Beast", "Room 1234", "MWF 10:00-11:00 AM");
+std::map<std::string, std::shared_ptr<Course>> courses = {
+    {"CS101", course1},
+    {"CS102", course2}
+};
+std::map<std::string, std::shared_ptr<Course>> courses2 = {
+    {"CS999", course3}
+};
 
 class DepartmentUnitTests : public ::testing::Test {
  protected:
     static Department* testDepartment;
     static Department* testDepartment2;
+    static Department* testDepartment3;
 
     static void SetUpTestSuite() {
-        auto course1 = std::make_shared<Course>(150,
-        "Dr. Smith", "Room 101", "MWF 10:00-11:00 AM");
-        auto course2 = std::make_shared<Course>(200,
-        "Prof. Johnson", "Room 102", "TR 2:00-3:30 PM");
-        std::map<std::string, std::shared_ptr<Course>> courses = {
-            {"CS101", course1},
-            {"CS102", course2}
-        };
         testDepartment = new Department("CS", courses,
          "Dr. Jameson", 120);
         testDepartment2 = new Department();
+        testDepartment3 = new Department("CS", courses
+        , "Dr. Jameson", 120);
     }
 
     static void TearDownTestSuite() {
         delete testDepartment;
         delete testDepartment2;
+        delete testDepartment3;
     }
 };
 
 Department* DepartmentUnitTests::testDepartment = nullptr;
 Department* DepartmentUnitTests::testDepartment2 = nullptr;
+Department* DepartmentUnitTests::testDepartment3 = nullptr;
+
+TEST_F(DepartmentUnitTests, removePersonFromMajorTest) {
+    testDepartment->dropPersonFromMajor();
+    testDepartment2->dropPersonFromMajor();
+    ASSERT_EQ(119, testDepartment->getNumberOfMajors());
+    ASSERT_EQ(0, testDepartment2->getNumberOfMajors());
+}
 
 TEST_F(DepartmentUnitTests, AddPersonToMajorTest) {
     testDepartment2->addPersonToMajor();
@@ -38,7 +56,47 @@ TEST_F(DepartmentUnitTests, getDepartmentChairTest) {
     ASSERT_EQ("Dr. Jameson", testDepartment->getDepartmentChair());
 }
 
-TEST_F(DepartmentUnitTests, removePersonFromMajorTest) {
-    testDepartment->dropPersonFromMajor();
-    ASSERT_EQ(119, testDepartment->getNumberOfMajors());
+TEST_F(DepartmentUnitTests, displayTest) {
+     std::ostringstream expected;
+     expected << "CS CS101: \nInstructor: Dr. Smith; Location: Room 101; Time: MWF 10:00-11:00 AM\nCS CS102:"
+     <<" \nInstructor: Prof. Johnson; Location: Room 102; Time: TR 2:00-3:30 PM\n";
+    ASSERT_EQ(expected.str(), testDepartment->display());
 }
+
+TEST_F(DepartmentUnitTests, serializeAndDeserializeTest) {
+    std::string binaryFile = "testfile.bin";
+
+    static Department* originalDept = testDepartment3;
+    std::ofstream outFile(binaryFile, std::ios::binary);
+    ASSERT_TRUE(outFile.is_open()) << "Failed to open file for writing.";
+    originalDept->serialize(outFile);
+    outFile.close();
+
+    static Department* deserializedDept = new Department();
+    std::ifstream inFile(binaryFile, std::ios::binary);
+    ASSERT_TRUE(inFile.is_open()) << "Failed to open file for reading.";
+    deserializedDept->deserialize(inFile);
+    inFile.close();
+
+    EXPECT_EQ(originalDept->display(), deserializedDept->display());
+    EXPECT_EQ(originalDept->getDepartmentChair(), deserializedDept->getDepartmentChair());
+    EXPECT_EQ(originalDept->getNumberOfMajors(), deserializedDept->getNumberOfMajors());
+
+    std::remove(binaryFile.c_str());
+}
+
+TEST_F(DepartmentUnitTests, getCourseSelectionTest) {
+    std::map<std::string, std::shared_ptr<Course>> expectedMap = {
+            {"CS101", course1},
+            {"CS102", course2}
+        };
+    ASSERT_EQ(expectedMap, testDepartment->getCourseSelection());
+}
+
+TEST_F(DepartmentUnitTests, createAndAddCourseTest) {
+    auto courseSize = testDepartment2->getCourseSelection().size();
+    testDepartment2->createCourse("CS999", "Mr. Beast", "Room 1234", "MWF 10:00-11:00 AM", 150);
+    ASSERT_LE(courseSize, testDepartment2->getCourseSelection().size());
+}
+
+
